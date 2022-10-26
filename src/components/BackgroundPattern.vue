b13ab789469c1632e79a7ac7bfb36bb8ea33b849
hyeongrok7874 <khr5k0228@gmail.com>
2022-10-26 13:17:10 +0900

    Add BackgroundPattern

---
+++ src/components/BackgroundPattern.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="circle"
+    v-for="(_, i) in 100"
+    :key="i"
+    :style="circlePosition()"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundPattern",
+  methods: {
+    circlePosition() {
+      const innerWidth = window.innerWidth;
+      const innerHeight = window.innerHeight;
+      const leftRand = Math.random() * innerWidth;
+      const topRand = Math.random() * innerHeight;
+      const size = Math.random() * 100;
+      return {
+        width: `${size}px`,
+        height: `${size}px`,
+        left: `${leftRand}px`,
+        top: `${topRand}px`,
+      };
+    },
+  },
+};
+</script>
+
+<style></style>
